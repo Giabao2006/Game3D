5d5e725616e72a9fafc3917fd48a398d842cefd4
Giabao2006 <baone1908@gmail.com>
2025-09-12 19:37:07 +0700

    cc

--- Assets/Script/PointCollect.cs
+++ Assets/Script/PointCollect.cs
@@ -6,6 +6,7 @@ public class PointCollect : MonoBehaviour
 {
     private int point = 0;
     public TextMeshProUGUI pointText;
+    public GameObject fxPrefab; // prefab hiệu ứng FX
     void Start()
     {
 
@@ -21,11 +22,19 @@ public class PointCollect : MonoBehaviour
         point += p;
         UpdatePoint();
     }
+    void SpawnFX()
+    {
+        if (fxPrefab != null)
+        {
+            Instantiate(fxPrefab, transform.position, Quaternion.identity);
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             PlusPoint(1);
+            SpawnFX();
             Destroy(gameObject);
         }
     }
